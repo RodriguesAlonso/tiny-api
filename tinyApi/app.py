339d52ec6b3bf2b2159eec2386871ac5f9798aa8
Alonso <joaoalonso42@gmail.com>
2023-09-28 14:38:57 -0300

    clean code

--- tinyApi/app.py
+++ tinyApi/app.py
@@ -1,20 +1,9 @@
 from flask import Flask
-from ext.db import init_db, Serach, where
+from ext.db import init_db
 from ext.api import api
-from pprint import pprint
-
-import json
-
 
 def create_app():
     app = Flask(__name__)
-
-    @app.route("/")
-    def index():
-        return "<h1>Start the word</h1"
-
     database = init_db()
-    # pprint(database.search(where('realm') =='Tolariano'))
     api.init_app(app)
-
     return app
